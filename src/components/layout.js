ccf2aa0e805bd16a3bfe907f375bbe2b2207a09b
heytitle <pat.chormai@gmail.com>
2019-06-23 23:08:19 +0700

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,13 +25,11 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
+          paddingTop: `1rem`,
         }}
       >
         <main>{children}</main>
