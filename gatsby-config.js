0fac480c14870b0cb0cae4a1c99572b52972ef85
heytitle <pat.chormai@gmail.com>
2019-07-05 09:41:55 +0700

    use google-font

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `kanit`,
+          `athiti`
+        ],
+        display: 'swap'
+      }
+    }
   ],
 }
